c3ae671c888a0b47b592a8608d7ba0eee0c09d20
saisree <saisree@thoughtworks.com>
2018-05-16 11:04:51 +0530

    Use arrayOf and toInt/toChar functions

---
+++ src/kotlin1.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println("3.14 to Int: "+ (3.14.toInt()))
+    println("A to Int: "+ ('A'.toInt()))
+    println("65 to char: "+ 65.toChar())
+
+    val name = "saisree";
+    val longString = "my name is saisree";
+    println(longString.length);
+
+    var myArray = arrayOf(1, 1.23, "Doug");
+    println(myArray[2])
+}
